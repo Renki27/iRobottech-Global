aab41e8b0c7db697ecc5f88480ba682b7e3b344f
LAPTOP-PQVQE3LE\aleli <jose.lizano@ucrso.info>
2018-11-24 15:34:07 -0600

    Push prueba

--- irobottech-site/src/pages/About.js
+++ irobottech-site/src/pages/About.js
@@ -8,7 +8,7 @@ class About extends Component {
                 <div className="container">
                     <h2>About</h2>
                     <p>
-                        asdasdsa sad asd 3q3 hdqwd qwd jqwhbduyqwdg quwegdqwu eyg qweuy qwue quwe wq qwute uqwet wuqetquwetqwu uw qwuet quwet
+                       123 asdasdsa sad asd 3q3 hdqwd qwd jqwhbduyqwdg quwegdqwu eyg qweuy qwue quwe wq qwute uqwet wuqetquwetqwu uw qwuet quwet
                         qweigqsd saydg uyasgduyas gsad askd asg jasdbas d baksdb jhads jasbdjsadb ajsdb jashbdjasdvv asdv gasduwd saud gasud3
                         asdig gaisdg ausydg uasdg uasgd uaysgduy gayguyg uyguy gusyag dqg8wd gasd 8gasd 8g8q2 gd87wgdqwgdasidg isa7gd as7isas
                 </p>
